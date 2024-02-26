b7e17c34776d1a9bab1ced4d62386972a4f4082c
openmpy <suhwan@kakao.com>
2024-02-26 21:31:09 +0900

    feat: 대출 내역 조회 기능

--- src/main/java/com/example/mission02/domain/loan/dto/LoanResponseDto.java
+++ src/main/java/com/example/mission02/domain/loan/dto/LoanResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.mission02.domain.loan.dto;
 
+import com.example.mission02.domain.book.entity.Book;
 import com.example.mission02.domain.loan.entity.Loan;
+import com.example.mission02.domain.user.entity.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
@@ -50,6 +52,34 @@ public class LoanResponseDto {
             this.id = loan.getId();
             this.bookId = loan.getBookId();
             this.userId = loan.getUserId();
+            this.isReturned = loan.isReturned();
+            this.loanedAt = loan.getLoanedAt();
+            this.returnedAt = loan.getReturnedAt();
+        }
+    }
+
+    @Getter
+    public static class GetLoanResponseDto {
+
+        private final Long id;
+        private final String title;
+        private final String author;
+        private final String name;
+        private final String phone;
+        private final boolean isReturned;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private final LocalDateTime loanedAt;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private final LocalDateTime returnedAt;
+
+        public GetLoanResponseDto(Loan loan, Book book, User user) {
+            this.id = loan.getId();
+            this.title = book.getTitle();
+            this.author = book.getAuthor();
+            this.name = user.getName();
+            this.phone = user.getPhone();
             this.isReturned = loan.isReturned();
             this.loanedAt = loan.getLoanedAt();
             this.returnedAt = loan.getReturnedAt();
