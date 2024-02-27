0c42904dc279bac05e83bf642a5bad2a51e329d8
uha9218 <uha0542@naver.com>
2024-02-27 17:10:24 +0900

    feat: 중복 회원 확인 기능

--- src/main/java/com/example/mission02/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/mission02/domain/user/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.mission02.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+     boolean existsByPhone(String phone);
+     boolean existsByIdentification(String identification);
 }
--- src/main/java/com/example/mission02/domain/user/service/UserService.java
+++ src/main/java/com/example/mission02/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.mission02.domain.user.dto.UserRequestDto.CreateUserRequestDto
 import com.example.mission02.domain.user.dto.UserResponseDto.CreateUserResponseDto;
 import com.example.mission02.domain.user.entity.User;
 import com.example.mission02.domain.user.repository.UserRepository;
+import com.example.mission02.global.handler.exception.CustomApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    public CreateUserResponseDto CreateUser(CreateUserRequestDto RequestDto){
-        User user = userRepository.save(RequestDto.toEntity());
+    public CreateUserResponseDto CreateUser(CreateUserRequestDto requestDto){
+        if(userRepository.existsByPhone(requestDto.getPhone()) || userRepository.existsByIdentification(requestDto.getIdentification())){
+            throw new CustomApiException("이미 등록된 회원입니다.");
+        }
+        User user = userRepository.save(requestDto.toEntity());
         return new CreateUserResponseDto(user);
     }
 }
