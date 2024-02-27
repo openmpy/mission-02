0398926d41dacdc8414bf0b044ee30ecc0ea7436
uha9218 <uha0542@naver.com>
2024-02-27 15:34:22 +0900

    feat: 선택한 도서 정보 조회 기능

--- src/main/java/com/example/mission02/domain/book/service/BookService.java
+++ src/main/java/com/example/mission02/domain/book/service/BookService.java
@@ -1,13 +1,15 @@
 package com.example.mission02.domain.book.service;
 
 import com.example.mission02.domain.book.dto.BookRequestDto.CreateBookRequestDto;
-import com.example.mission02.domain.book.dto.BookResponseDto;
-import com.example.mission02.domain.book.dto.BookResponseDto.GetBookResponseDto;
 import com.example.mission02.domain.book.dto.BookResponseDto.CreateBookResponseDto;
+import com.example.mission02.domain.book.dto.BookResponseDto.GetBookResponseDto;
 import com.example.mission02.domain.book.entity.Book;
 import com.example.mission02.domain.book.repository.BookRepository;
+import com.example.mission02.global.handler.exception.CustomApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,11 +21,18 @@ public class BookService {
         Book book = bookRepository.save(requestDto.toEntity());
         return new CreateBookResponseDto(book);
     }
-    
+    @Transactional(readOnly = true)
     public List<GetBookResponseDto> getBookList(){
         return bookRepository.findAllByOrderByCreatedAtAsc()
                 .stream()
                 .map(GetBookResponseDto::new)
                 .toList();
+    }
+    @Transactional(readOnly = true)
+    public GetBookResponseDto getBook(Long id){
+        Book book= bookRepository.findById(id).orElseThrow(()->
+            new CustomApiException("찾는 책이 없습니다.")
+        );
+        return new GetBookResponseDto(book);
     }
 }
