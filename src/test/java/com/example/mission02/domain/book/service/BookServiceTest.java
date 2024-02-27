13db1e102a27abc113943f83252ba930ac85a458
uha9218 <77494780+uha9218@users.noreply.github.com>
2024-02-27 19:45:27 +0900

    feat: 도서 대출 가능 여부 기능 (#29)
    * feat: 도서 대출 가능 여부 기능
    
    * test: 도서 대출 가능 여부 Test

--- src/test/java/com/example/mission02/domain/book/service/BookServiceTest.java
+++ src/test/java/com/example/mission02/domain/book/service/BookServiceTest.java
@@ -5,6 +5,14 @@ import com.example.mission02.domain.book.dto.BookResponseDto.CreateBookResponseD
 import com.example.mission02.domain.book.dto.BookResponseDto.GetBookResponseDto;
 import com.example.mission02.domain.book.entity.Book;
 import com.example.mission02.domain.book.repository.BookRepository;
+import com.example.mission02.domain.loan.dto.LoanRequestDto.CreateLoanRequestDto;
+import com.example.mission02.domain.loan.dto.LoanResponseDto;
+import com.example.mission02.domain.loan.entity.Loan;
+import com.example.mission02.domain.loan.repository.LoanRepository;
+import com.example.mission02.domain.loan.service.LoanService;
+import com.example.mission02.domain.user.entity.GenderType;
+import com.example.mission02.domain.user.entity.User;
+import com.example.mission02.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,10 +23,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.LongStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 
@@ -27,8 +37,17 @@ class BookServiceTest {
     @InjectMocks
     private BookService bookService;
 
+    @InjectMocks
+    private LoanService loanService;
+
     @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private LoanRepository loanRepository;
 
     @Test
     @DisplayName("성공 - 책을 등록한다.")
@@ -119,5 +138,43 @@ class BookServiceTest {
         List<GetBookResponseDto> responseDtoList = bookService.getBookList();
         Assertions.assertEquals(3, responseDtoList.size());
         Assertions.assertEquals("2", responseDtoList.get(2).getAuthor());
+    }
+
+    @Test
+    @DisplayName("성공 - 도서 대출 가능 여부를 확인한다.")
+    void checkIsLoaned() throws Exception {
+        Book book = Book.builder()
+                .id(1L)
+                .title("Book 1")
+                .author("Author 1")
+                .language("English")
+                .publisher("Publisher 1")
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .name("name")
+                .gender(GenderType.FEMALE)
+                .phone("01000000000")
+                .identification("123")
+                .address("")
+                .build();
+
+        Loan loan = Loan.builder()
+                .id(1L)
+                .book(book)
+                .user(user)
+                .build();
+
+        CreateLoanRequestDto requestDto = new CreateLoanRequestDto(book.getId(), user.getId());
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(book));
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        when(loanRepository.existsByUserAndIsReturnedFalse(any())).thenReturn(false);
+        when(loanRepository.save(any())).thenReturn(requestDto.toEntity(book, user));
+
+        LoanResponseDto.CreateLoanResponseDto responseDto = loanService.create(requestDto);
+        GetBookResponseDto bookResponseDto = bookService.getBook(book.getId());
+
+        Assertions.assertTrue(bookResponseDto.isLoaned());
     }
 }
